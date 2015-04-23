15b60fa0e540d6e4bfd193d5f655568a7ecb7a2d
Scott Dodson <sdodson@redhat.com>
2015-04-23 16:21:37 -0400

    Hog some CPU by computing 100000 digits of pi

--- app.rb
+++ app.rb
@@ -3,6 +3,25 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port, 8080
 
+SCALE = 10000  
+ARRINIT = 2000  
+def pi_digits(digits)  
+  pi = ""  
+  carry = 0  
+  arr = Array.new(digits + 1, ARRINIT)  
+  digits.step(1, -14) do |i|  
+    sum = 0  
+    i.downto(1) do |j|  
+      sum = sum * j + SCALE * arr[j]  
+      arr[j] = sum % (j * 2 - 1)  
+      sum /= j * 2 - 1  
+    end  
+    pi << sprintf("%04d", carry + sum / SCALE)  
+    carry = sum % SCALE;  
+  end  
+  pi  
+end 
+
 get '/' do
-  "Hello, Sinatra!"
+  puts pi_digits(100000)
 end
